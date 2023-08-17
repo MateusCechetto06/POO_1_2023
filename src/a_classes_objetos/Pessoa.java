c9fa7d25ce7d1386a968d72d161aab59615e0298
MateusCechetto06 <mateuscechetto06@gmail.com>
2023-08-17 19:17:35 -0300

    Commit relacionado a inserção dos arquivos do projeto

---
+++ src/a_classes_objetos/Pessoa.java
@@ -0,0 +1,61 @@
+package a_classes_objetos;
+
+import javax.swing.JOptionPane;
+
+public class Pessoa {
+	
+	String nome;
+	double peso;
+	double altura;
+	char sexo;
+
+	void cadastra() {
+		nome = JOptionPane.showInputDialog("Informe o nome:");
+		peso = Double.parseDouble(JOptionPane.showInputDialog("Peso:"));
+		altura = Double.parseDouble(JOptionPane.showInputDialog("Altura:"));
+		sexo = JOptionPane.showInputDialog("Sexo: (M/F)").toUpperCase().charAt(0);
+	}
+	
+	double calculaImc() {
+		return peso / (altura * altura);
+	}
+	
+	String avaliacao() {
+		String avaliacao = "";
+		double imc = calculaImc();
+		
+		if(sexo == 'M') {
+			if(imc < 20.7) {
+				avaliacao = "Abaixo do peso";
+			}else if(imc >= 20.7 && imc <= 26.4) {
+				avaliacao = "Peso ideal";
+			}else if(imc > 26.4 && imc <= 27.8) {
+				avaliacao = "Pouco acima do peso";
+			}else if(imc > 27.8 && imc <= 31.1) {
+				avaliacao = "Acima do peso";
+			}else if(imc > 31.1) {
+				avaliacao = "Obesidade";
+			}
+		}else if(sexo == 'M') {
+			if(imc < 19.1) {
+				avaliacao = "Abaixo do peso";
+			}else if(imc >= 19.1 && imc <= 25.8) {
+				avaliacao = "Peso ideal";
+			}else if(imc > 25.8 && imc <= 27.3) {
+				avaliacao = "Pouco acima do peso";
+			}else if(imc > 27.3 && imc <= 32.3) {
+				avaliacao = "Acima do peso";
+			}else if(imc > 32.3) {
+				avaliacao = "Obesidade";
+			}
+		}
+		
+		return avaliacao;
+	}
+	
+	String exibirDados() {
+		return "Nome: " + nome + "\n"
+             + "IMC: " + calculaImc() + "\n"
+             + "Categoria: " + avaliacao()  + "\n\n";
+	}
+}
